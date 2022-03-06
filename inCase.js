51a4e5a0b979605d23bcf467ec862d5a4cbc0df5
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-03-06 15:44:19 -0500

    update

---
+++ inCase.js
@@ -0,0 +1,30 @@
+// const fs = require('fs')
+// const inquirer = require('inquirer')
+// const Handlebars = require('handlebars')
+// const template = fs.readFileSync('src/template.hbs', 'utf8').toString()
+
+// const data = [
+//     {
+//         name: 'Jared',
+//         role: 'Manager',
+//         id: 1123,
+//         email: 'jared1234@gmail.com'
+//     },
+//     {
+//         name: 'Tom',
+//         role: 'Engineer',
+//         id: 1152,
+//         email: 'tom1212@gmail.com'
+
+//     },
+
+// ]
+
+
+
+
+
+// const compiledTemplate = Handlebars.compile(template)
+// const renderedHtml = compiledTemplate(data)
+// fs.writeFileSync('dist/index.html', renderedHtml, 'utf8')
+
--- index.js
+++ index.js
@@ -4,9 +4,37 @@ const inquirer = require('inquirer')
 const Handlebars = require('handlebars')
 const template = fs.readFileSync('src/template.hbs', 'utf8').toString()
 
+
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the managers name?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "Enter the manager's ID number",
+            name: "managerId"
+        },
+        {
+            type: "input",
+            message: "Enter the manager's email",
+            name: "managerEmail"
+        },
+        {
+            type: "input",
+            message: "Enter the manager's office number",
+            name: "managerPhone",
+    
+        },
+    ])
+};
+
+
 const data = [
     {
-        name: 'Jared',
+        name: managerName.value,
         role: 'Manager',
         id: 1123,
         email: 'jared1234@gmail.com'
@@ -21,10 +49,7 @@ const data = [
 
 ]
 
-
-
-
-
+//Links to Handlebars and HTML file to be rendered
 const compiledTemplate = Handlebars.compile(template)
 const renderedHtml = compiledTemplate(data)
 fs.writeFileSync('dist/index.html', renderedHtml, 'utf8')
